98c7266c43ea8f7df6824286e89d06f55a9ecc4d
cagatay-usta <cagatay_usta@windowslive.com>
2023-07-10 23:18:01 +0300

    refactor inputButtons component

--- src/components/inputButtons.js
+++ src/components/inputButtons.js
@@ -1,39 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-// add and delete buttons
-// adds a new item to category list with keys with empty values
-
-// get category, copy the keys, add new item with those keys
-
-export default class InputButton extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { type, category, buttonHandler } = this.props;
-    if (type === "add") {
-      return (
-        <button
-          type="button"
-          className="add-button"
-          data-category={`add-${category}`}
-          onClick={(e) => buttonHandler(e)}
-        >
-          Add
-        </button>
-      );
-    } else if (type === "delete") {
-      return (
-        <button
-          type="button"
-          className="delete-button"
-          data-category={`delete-${category}`}
-          onClick={(e) => buttonHandler(e)}
-        >
-          Delete
-        </button>
-      );
-    } else return null;
-  }
+export default function InputButton(props) {
+  const { type, category, buttonHandler } = props;
+  if (type === "add") {
+    return (
+      <button
+        type="button"
+        className="add-button"
+        data-category={`add-${category}`}
+        onClick={(e) => buttonHandler(e)}
+      >
+        Add
+      </button>
+    );
+  } else if (type === "delete") {
+    return (
+      <button
+        type="button"
+        className="delete-button"
+        data-category={`delete-${category}`}
+        onClick={(e) => buttonHandler(e)}
+      >
+        Delete
+      </button>
+    );
+  } else return null;
 }
